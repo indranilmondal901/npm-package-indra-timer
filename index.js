6230306685b84169373740c20598ff4e1f37566e
indranilmondal901 <122368872+indranilmondal901@users.noreply.github.com>
2024-03-10 17:27:32 +0530

    indra-timer package is ready for publishing

---
+++ index.js
@@ -0,0 +1,78 @@
+const moment = require('moment');
+
+class Timer {
+  constructor() {
+    this.startTime = null;
+    this.endTime = null;
+    this.pausedTime = 0;
+    this.isRunning = false;
+  }
+
+  start(hours, minutes, seconds) {
+    if (this.isRunning) {
+      console.error('Timer is already running.');
+      return;
+    }
+
+    this.startTime = moment();
+    this.endTime = moment().add({ hours, minutes, seconds });
+    this.isRunning = true;
+
+    this.intervalId = setInterval(() => {
+      const remainingTime = this.calculateRemainingTime();
+      console.log(this.formatTime(remainingTime));
+
+      if (remainingTime <= 0) {
+        this.stop();
+      }
+    }, 1000);
+  }
+
+  pause() {
+    if (!this.isRunning) {
+      console.error('Timer is not running.');
+      return;
+    }
+
+    clearInterval(this.intervalId);
+    this.pausedTime = this.calculateRemainingTime();
+    this.isRunning = false;
+  }
+
+  resume() {
+    if (this.isRunning) {
+      console.error('Timer is already running.');
+      return;
+    }
+
+    this.endTime = moment().add(this.pausedTime, 'seconds');
+    this.isRunning = true;
+
+    this.intervalId = setInterval(() => {
+      const remainingTime = this.calculateRemainingTime();
+      console.log(this.formatTime(remainingTime));
+
+      if (remainingTime <= 0) {
+        this.stop();
+      }
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.isRunning = false;
+    console.log('Timer stopped.');
+  }
+
+  calculateRemainingTime() {
+    return Math.max(0, this.endTime.diff(moment(), 'seconds'));
+  }
+
+  formatTime(seconds) {
+    const duration = moment.duration(seconds, 'seconds');
+    const formattedTime = moment.utc(duration.asMilliseconds()).format('HH:mm:ss');
+    return formattedTime;
+  }
+}
+
+module.exports = Timer;
